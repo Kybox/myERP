c73ea7b7649b2c3fbcc53e0b123771059d80ef9e
Kybox <nslr@riseup.net>
2018-10-26 13:31:58 +0200

    Add Consumer unit tests

---
+++ src/myerp-consumer/src/test/java/com/dummy/myerp/consumer/dao/impl/db/dao/ComptabiliteDaoImplTest.java
@@ -0,0 +1,76 @@
+package com.dummy.myerp.consumer.dao.impl.db.dao;
+
+import com.dummy.myerp.model.bean.comptabilite.CompteComptable;
+import com.dummy.myerp.model.bean.comptabilite.EcritureComptable;
+import com.dummy.myerp.model.bean.comptabilite.JournalComptable;
+import com.dummy.myerp.technical.exception.NotFoundException;
+import com.dummy.myerp.testconsumer.consumer.ConsumerTestCase;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Kybox
+ * @version 1.0
+ */
+public class ComptabiliteDaoImplTest extends ConsumerTestCase {
+
+    private ComptabiliteDaoImpl comptabiliteDao;
+    private EcritureComptable ecritureComptable;
+    private List<String> listReferences;
+
+    @Before
+    public void setUp(){
+
+        comptabiliteDao = new ComptabiliteDaoImpl();
+
+        getReferences();
+    }
+
+    private void getReferences(){
+
+        listReferences = new ArrayList<>();
+        for(EcritureComptable ecriture : comptabiliteDao.getListEcritureComptable())
+            listReferences.add(ecriture.getReference());
+    }
+
+    @Test
+    public void getListQueries() {
+
+        List<?> list;
+
+        list = comptabiliteDao.getListEcritureComptable();
+        Assert.assertTrue(list.size() >= 5);
+
+        list = comptabiliteDao.getListCompteComptable();
+        Assert.assertTrue(list.size() >= 7);
+
+        list = comptabiliteDao.getListJournalComptable();
+        Assert.assertTrue(list.size() >= 4);
+    }
+
+    @Test
+    public void getEcritureComptable() throws NotFoundException {
+
+        ecritureComptable = comptabiliteDao.getEcritureComptable(-1);
+        Assert.assertEquals(listReferences.get(0), ecritureComptable.getReference());
+
+        ecritureComptable = comptabiliteDao.getEcritureComptable(-3);
+        Assert.assertNotEquals(listReferences.get(1), ecritureComptable.getReference());
+
+        for(int i = 0; i < listReferences.size(); i++){
+            ecritureComptable = comptabiliteDao.getEcritureComptableByRef(listReferences.get(i));
+
+            String journalCode = ecritureComptable.getReference().substring(0,2);
+            Assert.assertEquals(ecritureComptable.getJournal().getCode(), journalCode);
+
+            String ecritureDate = new SimpleDateFormat("yyyy").format(ecritureComptable.getDate());
+            String dateRef = ecritureComptable.getReference().substring(3,7);
+            Assert.assertEquals(ecritureDate, dateRef);
+        }
+    }
+}
